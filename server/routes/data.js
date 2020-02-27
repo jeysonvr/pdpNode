943a19a5e1105bb84c57dfe091be9b84adbb6be2
Jeyson <jeysonvegaromero@gmail.com>
2020-02-27 17:58:27 -0500

    Send mail

--- server/routes/data.js
+++ server/routes/data.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 const request = require('request');
 const iconv = require('iconv-lite');
 const axios = require('axios');
-var csv = require('csv-express');
+// var csv = require('csv-express');
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -89,7 +89,7 @@ app.post('/', (req, res) => {
                         attachments: [
                             {
                                 filename: 'text1.csv',
-                                content: data
+                                content: csv(data)
                             }
                         ]
                         // bcc: "fred@gmail.com"
